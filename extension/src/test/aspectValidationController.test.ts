7f0a8e6664e4fd3c5f1b2f388a551d6288c72a7c
Andreas Textor <Andreas.Textor@de.bosch.com>
2026-06-11 09:07:49 +0200

    Update copyright headers

--- extension/src/test/aspectValidationController.test.ts
+++ extension/src/test/aspectValidationController.test.ts
@@ -1,3 +1,14 @@
+// Copyright (c) 2026 Robert Bosch Manufacturing Solutions GmbH
+//
+// See the AUTHORS file(s) distributed with this work for additional
+// information regarding authorship.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https:gmozilla.org/MPL/2.0/.
+//
+// SPDX-License-Identifier: MPL-2.0
+
 import * as assert from 'assert';
 import * as vscode from 'vscode';
 import {AspectValidationController, VALIDATE_DOCUMENT_REQUEST} from '../aspectValidation';
